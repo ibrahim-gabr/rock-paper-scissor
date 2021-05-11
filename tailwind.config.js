49356197d725c09f652a3cf84d2efda5eef248c3
Ibrahim Abdallah <iageoscience@live.com>
2021-05-11 11:57:57 +0200

    delete build directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 
 module.exports = {
-  purge: ["./src/**/*.svelte", "./src/**/*.html"],
+  purge: ["./src/**/*.svelte"],
   theme: {
     extend: {
       fontFamily: {
